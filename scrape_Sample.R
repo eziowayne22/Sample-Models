485884fbb118226f986f7df14905830d1c2feb2d
eziowayne22 <53155065+eziowayne22@users.noreply.github.com>
2022-07-07 12:53:23 -0400

    Update scrape_Sample.R
    remove views

--- scrape_Sample.R
+++ scrape_Sample.R
@@ -7,15 +7,9 @@
 library(tidyverse)
 library(rvest)
 library(lubridate)
-
-install.packages("sqldf")
-library(sqldf)
-install.packages("taskscheduleR")
-
 ---------------------------------------------------------------------------------------
 #given a link, read_html gives you the html source code for the page
 rogue_link <-  read_html("https://www.roguefitness.com/weightlifting-bars-plates/barbells")
-View(rogue_link)
 #use selector gadget to get the element desired, pipe to html text to extract that text
 rogue_name <-  rogue_link %>% html_nodes(".title") %>% html_text()
 View(rogue_name)
@@ -32,7 +26,6 @@ rogue_price <- rogue_link %>% html_nodes(".price") %>% html_text()
 
 #Provide the current date when it runs
 date_of_extract <- as.data.frame(as.Date(Sys.Date()))
-View(date_of_extract)
 test <- bind_cols(rogue_name_3,rogue_price_2,date_of_extract) %>% 
   rename(new_date="as.Date(Sys.Date())")
 
@@ -42,8 +35,6 @@ df_rogue <- as.data.frame(test) %>%
   #mutate(new_date=as.character(date_of_extract)) %>% 
   arrange(desc(numeric_price)) %>% 
   select(new_date,rogue_name,rogue_price)
-
-View(df_rogue)
 
 #Record the output
 write.table(df_rogue, paste("Rogue_Barbell_",toString(Sys.Date()),".csv"),row.names=F, sep=",")
